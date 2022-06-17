6013b3e534f6276b28548f8630a2b8554570af65
lvmt <13554221497@163.com>
2022-06-17 17:15:23 +0800

    固定图例位置信息

--- pipeline_metaware/QC/bin/plot.py
+++ pipeline_metaware/QC/bin/plot.py
@@ -6,6 +6,7 @@
 '''
 
 # 质控图片绘制
+import os 
 import sys  
 import json 
 import pandas as pd 
@@ -15,6 +16,7 @@ plt.switch_backend('agg')
 
 json_file = sys.argv[1]
 out_suffix = json_file.replace('.fastp.json', '')
+name = os.path.basename(out_suffix)
 
 info = json.load(open(json_file, 'r'))
 read1_quality_df = pd.DataFrame.from_dict(info['read1_after_filtering']['quality_curves'])
@@ -28,31 +30,35 @@ fig1_qua = read1_quality_df['mean'].plot()
 plt.ylim(0,40)
 plt.xlabel('position')
 plt.ylabel('quality')
-plt.title('read1')
+plt.title(f'{name} read1')
+plt.legend(loc='upper right')
 fig1_qua.figure.savefig(f'{out_suffix}.read1.qual.png', dpi=600)
 
 fig2_qua = read2_quality_df['mean'].plot()
 plt.ylim(0,40)
 plt.xlabel('position')
 plt.ylabel('quality')
-plt.title('read2')
+plt.title(f'{name} read2')
+plt.legend(loc='upper right')
 fig2_qua.figure.savefig(f'{out_suffix}.read2.qual.png', dpi=600)
 
 
 # GC含量绘图
 fig1_gc = read1_GC_df.plot()
-plt.ylim(0.1, 0.6)
+plt.ylim(-0.1, 0.6)
 plt.xlabel('position')
 plt.ylabel('percent')
-plt.title('read1')
+plt.title(f'{name} read1')
+plt.legend(loc='upper right')
 fig1_gc.figure.savefig(f'{out_suffix}.read1.GC.png', dpi=600)
 
-fig2_gc = read1_GC_df.plot()
-plt.ylim(0.1, 0.6)
+fig2_gc = read2_GC_df.plot()
+plt.ylim(-0.1, 0.6)
 plt.xlabel('position')
 plt.ylabel('percent')
-plt.title('read2')
-fig1_gc.figure.savefig(f'{out_suffix}.read2.GC.png', dpi=600)
+plt.title(f'{name} read2')
+plt.legend(loc='upper right')
+fig2_gc.figure.savefig(f'{out_suffix}.read2.GC.png', dpi=600)
 
 # # 生成统计文件
 # before_df = pd.DataFrame([info['summary']['before_filtering']]).rename(index={0:'before'})
