ace17e5d2f466480bac1693201bb121b9eb218cd
lvmt <13554221497@163.com>
2022-06-23 17:25:08 +0800

    rename

--- pipeline_metaware/GenePrediction/bin/merge_abundance_table.py
+++ pipeline_metaware/GenePrediction/bin/merge_abundance_table.py
@@ -25,7 +25,7 @@ args = vars(parser.parse_args())
 df_abundance_list = []
 df_gene_list = []
 for table in args['abundance_tables']:
-    name = os.path.basename(table)
+    name = os.path.basename(table).replace('.abundance.table', '')
     df = pd.read_csv(table, sep='\t')
     df_abundance = df.loc[:, ['gene', 'abundance']].rename(columns={'abundance': name})
     df_gene = df.loc[:, ['gene', 'count']].rename(columns={'count': name})
@@ -36,8 +36,8 @@ for table in args['abundance_tables']:
 combine_abundance_df = reduce(lambda x,y: pd.merge(x, y, on='gene', how='outer').fillna(0), df_abundance_list)
 combine_gene_df = reduce(lambda x,y: pd.merge(x, y, on='gene', how='outer').fillna(0), df_gene_list)
 
-merge_gene = '{merge_result_suffix}.combine.readsNum'.format(**args)
-merge_abudance = '{merge_result_suffix}.combine.readsNum.relative.xls'.format(**args)
+merge_gene = '{merge_result_suffix}.gene.readsNum'.format(**args)
+merge_abudance = '{merge_result_suffix}.abundance.relative.xls'.format(**args)
 
 combine_abundance_df.to_csv(merge_abudance, sep='\t', index=None)
 combine_gene_df.to_csv(merge_gene, sep='\t', index=None)
